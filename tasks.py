ee63eebe1a1bac05edd5d611700f1bdfbc6d8eb0
Nahuel Defossé <D3f0@users.noreply.github.com>
2025-09-17 13:38:45 +0300

    Checkpoint 2025-09-17 13:38:45.194752

--- tasks.py
+++ tasks.py
@@ -1,17 +1,24 @@
+import os
+from shutil import which
 import sys
 from invoke import task
 from invoke.context import Context
 from datetime import datetime
 
+
 @task()
-def preview(ctx: Context):
+def preview(ctx: Context, port: str = "$PORT"):
     """Build slides"""
-    ctx.run("quarto preview --render revealjs --port $PORT")
+    if which("entr"):
+        print("Not using entr for reload")
+    ctx.run(
+        "quarto preview --render revealjs --port $PORT", env={"PORT": os.getenv("PORT")}
+    )
 
 
 @task()
 def publish(ctx: Context):
-    """Build slides"""
+    """Publish slides"""
     ctx.run("quarto publish gh-pages --no-prompt")
 
 
@@ -26,5 +33,5 @@ def checkpoint(ctx: Context):
     ).stdout.splitlines():
         ctx.run(f"git add {f}")
     ctx.run("git add -u")
-    
+
     ctx.run(f"git commit -m 'Checkpoint {now}'")
